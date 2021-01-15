7fcf8b03a614f5b44eefe35e771eeb3483261ed7
kaique coimbra <kaique.caires@yahoo.com>
2021-01-15 14:28:12 -0300

    implements Array data structure

---
+++ src/data-structures/array.js
@@ -0,0 +1,45 @@
+class ArrayData {
+  constructor () {
+    this.length = 0
+    this.data = {}
+  }
+
+  get (index) {
+    return this.data[index]
+  }
+
+  push (item) {
+    this.data[this.length] = item
+    this.length++
+    return this.length
+  }
+
+  pop () {
+    const lastItem = this.data[this.length - 1]
+    delete this.data[this.length - 1]
+    this.length--
+    return lastItem
+  }
+
+  delete (index) {
+    const item = this.get(index)
+    this.shiftItems(index)
+    return item
+  }
+
+  shiftItems (index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1]
+    }
+
+    delete this.data[this.length - 1]
+    this.length--
+  }
+}
+
+const myArray = new ArrayData()
+
+myArray.push('a')
+myArray.push('b')
+
+console.log(myArray)
