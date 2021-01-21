81bbfab73711f29e1aa4e2b2b8d635cceb28decf
kaiquecaires <kaique.caires@yahoo.com>
2021-01-21 17:21:31 -0300

    implements breath first search algorithm

--- src/data-structures/binaryTree.js
+++ src/data-structures/binaryTree.js
@@ -93,6 +93,47 @@ class BinarySearchTree {
 
     return false
   }
+
+  breadthFirstSearch () {
+    let currentNode = this.root
+    let list = []
+    let queue = []
+    queue.push(currentNode)
+
+    while(queue.length > 0) {
+      currentNode = queue.shift()
+      list.push(currentNode.value)
+
+      if (currentNode.left) {
+        queue.push(currentNode.left)
+      }
+
+      if (currentNode.right) {
+        queue.push(currentNode.right)
+      }
+    }
+
+    return list
+  }
+
+  breadthFirstSearchRecursive (queue, list) {
+    if (!queue.length) {
+      return list
+    }
+
+    let currentNode = queue.shift()
+    list.push(currentNode.value)
+
+    if (currentNode.left) {
+      queue.push(currentNode.left)
+    }
+
+    if (currentNode.right) {
+      queue.push(currentNode.right)
+    }
+
+    return this.breadthFirstSearchRecursive(queue, list)
+  }
 }
 
 const tree = new BinarySearchTree()
@@ -103,7 +144,7 @@ tree.insert(20)
 tree.insert(170)
 tree.insert(15)
 tree.insert(1)
-console.log(tree.lookup(220))
+console.log(tree.breadthFirstSearchRecursive([tree.root], []))
 /*
         9
       4   20
