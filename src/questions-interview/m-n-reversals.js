8d6ff81c2aa1410d27fbe52c9c8a38fe5c265b5d
kaiquecaires <kaique.caires@yahoo.com>
2021-02-03 07:51:49 -0300

    solution: implements m-n-reversals

---
+++ src/questions-interview/m-n-reversals.js
@@ -0,0 +1,28 @@
+const mNReversals = (head, m, n) => {
+  let currentPos = 1, currentNode = head, start = head;
+
+  while (currentPos < m) {
+    start = currentNode;
+    currentNode = currentNode.next;
+    currentPos++;
+  }
+
+  let newList = null, tail = currentNode;
+
+  while (currentPos >= m && currentPos <= n) {
+    const next = currentNode.next;
+    currentNode.next = newList;
+    newList = currentNode;
+    currentNode = next;
+    currentPos++;
+  }
+
+  start.next = newList;
+  tail.next = currentNode;
+
+  if (m > 1) {
+    return head;
+  } else {
+    return newList;
+  }
+}
