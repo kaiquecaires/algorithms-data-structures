6541ae4b303ea7703e13653b7f76446a85572a0b
kaiquecaires <kaique.caires@yahoo.com>
2021-02-05 07:43:47 -0300

    solution: implements flatten doubly linked list solutions

--- src/questions-interview/flatten.js
+++ src/questions-interview/flatten.js
@@ -1,19 +1,31 @@
 class Node {
-  constructor (value , next = null, child = null) {
-    this.value = value;
-    this.next = next;
-    this.child = child;
-  } 
+  constructor (value) {
+    this.value = value
+    this.prev = null
+    this.next = null
+    this.child = null
+  }
 }
 
-let var6 = new Node(6)
-let var5 = new Node(5)
-let var4 = new Node(4, var5)
-let var3 = new Node(3, var6, var4)
-let var2 = new Node(2, var3)
-let var1 = new Node(1, var2)
+let var6 = new Node(6);
+let var5 = new Node(5);
+let var4 = new Node(4);
+let var3 = new Node(3);
+let var2 = new Node(2);
+let var1 = new Node(1);
+
+var1.next = var2;
+var2.next = var3;
+var2.prev = var1;
+var3.next = var6;
+var3.prev = var2;
+var3.child = var4;
+var4.next = var5;
+var5.prev = var4;
 
 const merge = (child, next) => {
+  if (!next) return child;
+
   let current = child;
 
   while (current.next) {
@@ -21,18 +33,24 @@ const merge = (child, next) => {
   }
 
   current.next = next;
+  next.prev = current;
+
   return child;
 }
 
 const flatten = (head) => {
   let current = head;
-  let list = []
 
   while (current) {
-    list.push(current.value)
     if (current.child) {
       const child = flatten(current.child);
-      current.next = merge(child, current.next);
+
+      if(child) {
+        const merged = merge(child, current.next);
+        current.next = merged;
+        merged.prev = current;
+      }
+
       current.child = null;
     }
 
@@ -42,4 +60,29 @@ const flatten = (head) => {
   return head;
 }
 
-flatten(null);
+//  Course solution
+const flattenV2 = (head) => {
+  if (!head) return head;
+  let currentNode = head;
+  while(currenNode !== null) {
+    if(currentNode.child === null) {
+      currentNode = currentNode.next;
+    } else {
+      let tail = currentNode.child;
+      while (tail.next !== null) {
+        tail = tail.next;
+      }
+      tail.next = currentNode.next;
+      if (tail.next !== null) {
+        tail.next.prev = tail;
+      }
+      currentNode.next = currentNode.child;
+      currentNode.next.prev = currentNode;
+      currentNode.child = null;
+    }
+  }
+
+  return head;
+}
+
+// Space O(1), Time O(n)
